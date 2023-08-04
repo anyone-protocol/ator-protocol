8a21d908aecf1d1044c1c1b5beacc8141bb78b00
David Goulet <dgoulet@torproject.org>
2023-08-04 10:04:36 -0400

    geoip: Fix cargo target directory
    Signed-off-by: David Goulet <dgoulet@torproject.org>

--- scripts/maint/geoip/update_geoip.sh
+++ scripts/maint/geoip/update_geoip.sh
@@ -18,7 +18,7 @@ location dump "$TMP/geoip-dump.txt"
 
 OLDDIR=$(pwd)
 cd "$DIR/geoip-db-tool/"
-cargo build --release
+cargo build --release --target-dir "target"
 cd "$OLDDIR"
 
 "$DIR/geoip-db-tool/target/release/geoip-db-tool" -i "$TMP/geoip-dump.txt"
