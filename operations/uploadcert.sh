72e99213e18215ad99e431638ec88d984a139d59
kanshi <46557+kanshi@users.noreply.github.com>
2024-01-17 12:13:06 +0100

    Fix typo

--- operations/uploadcert.sh
+++ operations/uploadcert.sh
@@ -1,12 +1,12 @@
 #arguments tor-data-folder node-id ip nickname
 
-consul kv put ator-network/stage/dir-aut-$2/authority_certificate $(cat keys/authority_certificate)
-consul kv put ator-network/stage/dir-aut-$2/ed25519_master_id_public_key_base64 $(base64 keys/ed25519_master_id_public_key)
-consul kv put ator-network/stage/dir-aut-$2/ed25519_signing_cert_base64 $(base64 keys/ed25519_signing_cert)
-consul kv put ator-network/stage/dir-aut-$2/fingerprint $(cat fingerprint)
-consul kv put ator-network/stage/dir-aut-$2/fingerprint-ed25519 $(cat fingerprint-ed25519)
-consul kv put ator-network/stage/dir-aut-$2/nickname $4
-consul kv put ator-network/stage/dir-aut-$2/public_ipv4 $3
+consul kv put ator-network/stage/dir-auth-$2/authority_certificate $(cat keys/authority_certificate)
+consul kv put ator-network/stage/dir-auth-$2/ed25519_master_id_public_key_base64 $(base64 keys/ed25519_master_id_public_key)
+consul kv put ator-network/stage/dir-auth-$2/ed25519_signing_cert_base64 $(base64 keys/ed25519_signing_cert)
+consul kv put ator-network/stage/dir-auth-$2/fingerprint $(cat fingerprint)
+consul kv put ator-network/stage/dir-auth-$2/fingerprint-ed25519 $(cat fingerprint-ed25519)
+consul kv put ator-network/stage/dir-auth-$2/nickname $4
+consul kv put ator-network/stage/dir-auth-$2/public_ipv4 $3
 
 vault kv put kv/ator-network/stage/dir-auth-$2
             authority_identity_key=$(cat keys/authority_identity_key) 
