5be19615d06920869269f3afb2c176e631f2b7ba
kanshi <46557+kanshi@users.noreply.github.com>
2024-08-12 12:16:12 +0200

    Adds certgen commands for stage

--- operations/run-gen-upload-cert.sh
+++ operations/run-gen-upload-cert.sh
@@ -3,6 +3,8 @@
 # consul env: CONSUL_HTTP_ADDR, CONSUL_HTTP_TOKEN, CONSUL_CACERT
 
 # STAGE by default in scripts
+
+## LIVE
 # bash gencert.sh da1 49.13.145.234 ATORDAeucstage
 # bash gencert.sh da2 5.161.108.187 ATORDAusestage
 # bash gencert.sh da3 5.78.90.106 ATORDAuswstage
@@ -11,11 +13,18 @@
 # bash gencert.sh da6 95.216.32.105 AnyoneHelLive
 # bash gencert.sh da7 176.9.29.53 AnyoneFalLive
 
+## STAGE
 bash gencert.sh stage-da1-dmz 88.99.219.105 AnyoneDmzStage
 bash gencert.sh stage-da2-fal 176.9.29.53 AnyoneFalStage
 bash gencert.sh stage-da3-hel 95.216.32.105 AnyoneHelStage
 
+bash gencert.sh stage-da2-fal-2 176.9.29.53 AnyoneFal2Stage
+bash gencert.sh stage-da3-hel-2 95.216.32.105 AnyoneHel2Stage
+bash gencert.sh stage-da2-fal-3 176.9.29.53 AnyoneFal2Stage
+bash gencert.sh stage-da3-hel-3 95.216.32.105 AnyoneHel2Stage
+
 # Check/update script for phase
+## LIVE
 # bash uploadcert.sh da1 067a42a8-d8fe-8b19-5851-43079e0eabb4 49.13.145.234 ATORDAeucstage
 # bash uploadcert.sh da2 16be0723-edc1-83c4-6c02-193d96ec308a 5.161.108.187 ATORDAusestage
 # bash uploadcert.sh da3 e6e0baed-8402-fd5c-7a15-8dd49e7b60d9 5.78.90.106 ATORDAuswstage
@@ -24,13 +33,20 @@ bash gencert.sh stage-da3-hel 95.216.32.105 AnyoneHelStage
 # bash uploadcert.sh da6 4aa61f61-893a-baf4-541b-870e99ac4839 95.216.32.105 AnyoneHelLive
 # bash uploadcert.sh da7 c2adc610-6316-cd9d-c678-cda4b0080b52 176.9.29.53 AnyoneFalLive
 
-bash uploadcert.sh stage-da1-dmz c8e55509-a756-0aa7-563b-9665aa4915ab 88.99.219.105 AnyoneDmzStage
-bash uploadcert.sh stage-da2-fal c2adc610-6316-cd9d-c678-cda4b0080b52 176.9.29.53 AnyoneFalLive
-bash uploadcert.sh stage-da3-hel 4aa61f61-893a-baf4-541b-870e99ac4839 95.216.32.105 AnyoneHelLive
+## STAGE
+bash uploadcert.sh stage-da1-dmz c8e55509-a756-0aa7-563b-9665aa4915ab-9101 88.99.219.105 AnyoneDmzStage
+bash uploadcert.sh stage-da2-fal c2adc610-6316-cd9d-c678-cda4b0080b52-9101 176.9.29.53 AnyoneFalLive
+bash uploadcert.sh stage-da3-hel 4aa61f61-893a-baf4-541b-870e99ac4839-9101 95.216.32.105 AnyoneHelLive
+
+bash uploadcert.sh stage-da2-fal-2 c2adc610-6316-cd9d-c678-cda4b0080b52-9102 176.9.29.53 AnyoneFalLive
+bash uploadcert.sh stage-da3-hel-2 4aa61f61-893a-baf4-541b-870e99ac4839-9102 95.216.32.105 AnyoneHelLive
+bash uploadcert.sh stage-da2-fal-3 c2adc610-6316-cd9d-c678-cda4b0080b52-9103 176.9.29.53 AnyoneFalLive
+bash uploadcert.sh stage-da3-hel-3 4aa61f61-893a-baf4-541b-870e99ac4839-9103 95.216.32.105 AnyoneHelLive
 
 # mind the phase.... and by default only checks curl output
 # Move DA folders and script to server, login, run script
 
+## LIVE
 # bash uploadsecrets.sh da1 067a42a8-d8fe-8b19-5851-43079e0eabb4
 # bash uploadsecrets.sh da2 16be0723-edc1-83c4-6c02-193d96ec308a
 # bash uploadsecrets.sh da3 e6e0baed-8402-fd5c-7a15-8dd49e7b60d9
@@ -39,6 +55,12 @@ bash uploadcert.sh stage-da3-hel 4aa61f61-893a-baf4-541b-870e99ac4839 95.216.32.
 # bash uploadsecrets.sh da6 4aa61f61-893a-baf4-541b-870e99ac4839
 # bash uploadsecrets.sh da7 c2adc610-6316-cd9d-c678-cda4b0080b52
 
-bash uploadsecrets.sh stage-da1-dmz c8e55509-a756-0aa7-563b-9665aa4915ab
-bash uploadsecrets.sh stage-da2-fal c2adc610-6316-cd9d-c678-cda4b0080b52
-bash uploadsecrets.sh stage-da3-hel 4aa61f61-893a-baf4-541b-870e99ac4839
+## STAGE
+bash uploadsecrets.sh stage-da1-dmz c8e55509-a756-0aa7-563b-9665aa4915ab-9101
+bash uploadsecrets.sh stage-da2-fal c2adc610-6316-cd9d-c678-cda4b0080b52-9101
+bash uploadsecrets.sh stage-da3-hel 4aa61f61-893a-baf4-541b-870e99ac4839-9101
+
+bash uploadsecrets.sh stage-da2-fal-2 c2adc610-6316-cd9d-c678-cda4b0080b52-9102
+bash uploadsecrets.sh stage-da3-hel-2 4aa61f61-893a-baf4-541b-870e99ac4839-9102
+bash uploadsecrets.sh stage-da2-fal-2 c2adc610-6316-cd9d-c678-cda4b0080b52-9103
+bash uploadsecrets.sh stage-da3-hel-2 4aa61f61-893a-baf4-541b-870e99ac4839-9103
