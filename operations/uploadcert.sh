bf49030b1e70d41a648039fd3444082d1b4a842b
kanshi <46557+kanshi@users.noreply.github.com>
2024-08-13 13:37:47 +0200

    Migrate dev away from live

--- operations/uploadcert.sh
+++ operations/uploadcert.sh
@@ -1,10 +1,10 @@
 #arguments folder node-id ip nickname
 
-consul kv put ator-network/stage/dir-auth-$2/authority_certificate "$(cat $1/tor-data/keys/authority_certificate)"
-consul kv put ator-network/stage/dir-auth-$2/ed25519_master_id_public_key_base64 "$(base64 -w 0 $1/tor-data/keys/ed25519_master_id_public_key)"
-consul kv put ator-network/stage/dir-auth-$2/ed25519_signing_cert_base64 "$(base64 -w 0 $1/tor-data/keys/ed25519_signing_cert)"
-consul kv put ator-network/stage/dir-auth-$2/fingerprint "$(cat $1/tor-data/fingerprint)"
-consul kv put ator-network/stage/dir-auth-$2/fingerprint-ed25519 "$(cat $1/tor-data/fingerprint-ed25519)"
-consul kv put ator-network/stage/dir-auth-$2/nickname "$4"
-consul kv put ator-network/stage/dir-auth-$2/public_ipv4 "$3"
+consul kv put ator-network/dev/dir-auth-$2/authority_certificate "$(cat $1/tor-data/keys/authority_certificate)"
+consul kv put ator-network/dev/dir-auth-$2/ed25519_master_id_public_key_base64 "$(base64 -w 0 $1/tor-data/keys/ed25519_master_id_public_key)"
+consul kv put ator-network/dev/dir-auth-$2/ed25519_signing_cert_base64 "$(base64 -w 0 $1/tor-data/keys/ed25519_signing_cert)"
+consul kv put ator-network/dev/dir-auth-$2/fingerprint "$(cat $1/tor-data/fingerprint)"
+consul kv put ator-network/dev/dir-auth-$2/fingerprint-ed25519 "$(cat $1/tor-data/fingerprint-ed25519)"
+consul kv put ator-network/dev/dir-auth-$2/nickname "$4"
+consul kv put ator-network/dev/dir-auth-$2/public_ipv4 "$3"
 
