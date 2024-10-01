guardianproject/tor

add Java Native Interface (JNI) API to support Android
Based mostly on @sysrqb's work aka TorApi.

The autotools stuff is based on:
https://www.gnu.org/software/autoconf-archive/ax_jni_include_dir.html

src/app/main/main.c
@@ -30,6 +30,9 @@
 #include "core/or/connection_or.h"
 #include "core/or/relay.h"
 #include "core/or/status.h"
+#ifdef HAVE_JNI_H
+#include "feature/api/org_torproject_jni_TorService.h"
+#endif
 #include "feature/api/tor_api.h"
 #include "feature/api/tor_api_internal.h"
 #include "feature/client/addressmap.h"

anyone-protocol/ator-protocol

src/app/main/main.c
@@ -34,6 +34,9 @@
 #include "core/or/connection_or.h"
 #include "core/or/relay.h"
 #include "core/or/status.h"
+#ifdef HAVE_JNI_H
+#include "feature/api/io_anyone_jni_AnonService.h"
+#endif
 #include "feature/api/tor_api.h"
 #include "feature/api/tor_api_internal.h"
 #include "feature/client/addressmap.h"