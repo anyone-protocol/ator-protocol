d903d014fe7d77a113791e27629e6f22380d9e57
Yurii Kovalchuk <103324562+web3yurii@users.noreply.github.com>
2025-01-13 09:38:10 +0200

    ANON-241 - Add anons file with default dns mapping (#116)

--- src/app/config/config.h
+++ src/app/config/config.h
@@ -47,6 +47,8 @@ int get_protocol_warning_severity_level(void);
 /** Pattern for backing up configuration files */
 #define CONFIG_BACKUP_PATTERN "%s.orig.1"
 
+#define DEFAULT_ANON_DNS_MAPPING "anyone.anon 6zctvi63m7xxbd34hxn2uvnaw5ao7sec4l3k4bflzeqtve5jlehz6tyd.anon"
+
 /** An error from options_trial_assign() or options_init_from_string(). */
 typedef enum setopt_err_t {
   SETOPT_OK = 0,
